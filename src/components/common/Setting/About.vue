dd6afa9cf233c853bf473b738b08622feea7d072
boyboy <66588374+1378056564@users.noreply.github.com>
2023-04-19 23:37:02 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -73,7 +73,7 @@ onMounted(() => {
 			<p>打赏账号</p>
 			<img src="https://img.juyuantop.cn/tBFuLi.jpg">
 			<p>微信群-里面有二狗子机器人接入了ChatGPT3.5</p>
-			<img src="https://img.juyuantop.cn/rYKQt6.jpg" alt="微信图片_20230408022951.jpg" title="微信图片_20230408022951.jpg" />
+			<img src="https://img.juyuantop.cn/Agv5JP.jpg" alt="微信图片_20230419233221.jpg" title="微信图片_20230419233221.jpg" />
     </div>
   </NSpin>
 </template>
